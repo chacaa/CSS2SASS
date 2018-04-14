881030699c8c55564465eb57c5e26c5ffcf20608
santiago <santiagocasaslopez@gmail.com>
2018-04-14 13:12:26 -0300

    some base code refactor

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const testFile = './test.css';
-const postcss = require('postcss');
-const fs = require('fs');
-const treeManager = require('./treeManager.js');
+const sassConverter = require('./src/sassConverter');
 
 module.exports = {
-    unSASS: function(file = testFile, allVariables) {
-        try {
-            let cssFile = fs.readFileSync(file, 'utf8');
-            let parsedCSS = postcss.parse(cssFile);
-            return treeManager.getSASSTree(parsedCSS, false);
-        } catch (error) {
-            return { 
-                variables: null, 
-                superClasses: null, 
-                tree: null, 
-                toString: null, 
-                error 
-            };
-        }
+    unSASS: function(file, allVariables) {
+        return sassConverter.unSASS(file, allVariables);
     }
 }
---
+++ src/sassConverter.js
@@ -0,0 +1,21 @@
+const postcss = require('postcss');
+const fs = require('fs');
+const treeManager = require('./treeManager.js');
+
+module.exports = {
+    unSASS: function(file, allVariables) {
+        try {
+            let cssFile = fs.readFileSync(file, 'utf8');
+            let parsedCSS = postcss.parse(cssFile);
+            return treeManager.getSASSTree(parsedCSS, false);
+        } catch (error) {
+            return { 
+                variables: null, 
+                superClasses: null, 
+                tree: null, 
+                toString: null, 
+                error 
+            };
+        }
+    }
+}
